51a164d40f237bfff6da0da638c6361358f8fe0c
Chase <chasestruse@gmail.com>
2020-11-25 20:39:44 -0600

    Finished the tutorial!

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react';
+
+const NotFound = () => (
+    <div>
+        <h1>Page Not Found</h1>
+        <p className="lead">The url you were looking for was not found... :(</p>
+    </div>
+)
+
+export default NotFound
---
+++ src/context/alert/AlertState.js
@@ -0,0 +1,26 @@
+import React, {useReducer} from "react";
+import AlertContext from './alertContext';
+import AlertReducer from './alertReducer';
+import {SET_ALERT, REMOVE_ALERT, SET_LOADING} from '../types';
+
+const AlertState = props => {
+    const initialState = null;
+
+    const [state, dispatch] = useReducer(AlertReducer, initialState);
+
+    const setLoading = () => dispatch({type: SET_LOADING});
+
+    const setAlert = (message, type) => {
+        dispatch({
+            type: SET_ALERT,
+            payload: {message, type}
+        })
+        setTimeout(() => dispatch({type: REMOVE_ALERT}), 3000);
+    };
+
+    return <AlertContext.Provider value={{ alert: state, setAlert}}>
+        {props.children}
+    </AlertContext.Provider>
+}
+
+export default AlertState
